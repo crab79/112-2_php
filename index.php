fb8c08aa88fc7c9a0f7ad51cfdf006e473c0214e
crab79 <hanyaoyao0709@gmail.com>
2024-02-22 18:05:57 +0800

    Week1 homework

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "hello php";
+
+        $a = 1;
+        $b = 2;
+
+        echo "<h1>hello world</h1>"
+    ?>
+    <h3>It is a h3</h3>
+
+</body>
+</html>
